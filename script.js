88fe393a1a8ffec01ebefc8d3605fe46701816c6
Jake Liu <13583702+LittleJake@users.noreply.github.com>
2022-08-01 20:20:30 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         华为ModelArt自动续期
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  华为ModelArt自动续期，适用于北京4免费节点。
+// @author       LittleJake
+// @match        https://authoring-modelarts-cnnorth4.huaweicloud.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=huaweicloud.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setInterval(function(){
+        document.querySelector('#lease-dur button').click();
+        document.querySelector('.MuiDialogActions-root button').click();
+        console.log("已续期")
+    }, 60*1000*10);
+})();
